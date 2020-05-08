c85ba1079d78265312e8716655d4a7af413ae1b7
Jezza <jezzadabomb@gmail.com>
2020-05-08 11:22:18 +0200

    Backup existing target file, if it already exists.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use std::collections::HashMap;
+use std::path::PathBuf;
 
+use gitlab::Gitlab;
 use serde::Deserialize;
 use structopt::StructOpt;
-use gitlab::Gitlab;
-use std::path::PathBuf;
 
 #[derive(StructOpt, Debug)]
 struct Args {
+	#[structopt(parse(from_os_str), long)]
+	config: Option<PathBuf>,
+
 	deploys: Vec<String>,
 }
 
@@ -29,9 +32,10 @@ struct Deploy {
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 	let args: Args = StructOpt::from_args();
-//	println!("{:#?}", args);
 
-	let config = {
+	let config = if let Some(config) = args.config {
+		config
+	} else {
 		let mut config = match dirs::config_dir() {
 			Some(config) => config,
 			None => {
@@ -49,68 +53,69 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 	}
 
 	let content = std::fs::read_to_string(&config)?;
-
 	let config = toml::from_str::<Config>(&content)?;
-//	println!("{:#?}", config);
 
 	let token = rpassword::read_password_from_tty(Some("Token: "))?;
 	if token.is_empty() {
 		eprintln!("No token provided.");
-		return Ok(())
+		return Ok(());
 	}
 
+	let gl = gitlab::GitlabBuilder::new(&config.host, &token)
+		.cert_insecure()
+		.build()?;
 
-	let gl = {
-		let host = config.host;
-
-		use gitlab::GitlabBuilder;
-		let mut builder = GitlabBuilder::new(host, &token);
-		builder.cert_insecure();
-		builder.build()?
-	};
-
-    for deploy in &args.deploys {
+	for deploy in &args.deploys {
 		let name = &*deploy;
-        if let Some(deploy) = config.deploy.get(name) {
-            download(&gl, config.output.as_deref(), name, deploy)?;
-        } else {
-            eprintln!("Unknown deploy key: {}", name);
-        }
-    }
+		if let Some(deploy) = config.deploy.get(name) {
+			download(&gl, config.output.as_deref(), name, deploy)?;
+		} else {
+			eprintln!("Unknown deploy key: {}", name);
+		}
+	}
 
 	Ok(())
 }
 
 fn download(gl: &Gitlab, output: Option<&str>, name: &str, deploy: &Deploy) -> Result<(), Box<dyn std::error::Error>> {
-
-	let path = if let Some(output) = deploy.output.as_deref().or(output) {
-		let mut path = PathBuf::new();
-		path.push(output);
-		path.push(&deploy.file);
-		path
-	} else {
-		eprintln!("No output folder specified for {}", name);
-		return Ok(());
+	let path = match deploy.output.as_deref().or(output) {
+		Some(output) => {
+			let mut path = PathBuf::from(output);
+			path.push(&deploy.file);
+			path
+		},
+		None => {
+			eprintln!("No output folder specified for {}", name);
+			return Ok(());
+		}
 	};
 
 	let project = gl.project_by_name::<_, Vec<(&'static str, &'static str)>, _, _>(&deploy.project, vec![])?;
 
-	let options = {
-		let mut opts = std::fs::OpenOptions::new();
-		opts.write(true);
-		opts.create(true);
-		opts.truncate(true);
-        opts
-	};
+	if path.exists() {
+		let backup = {
+			let mut backup = path.clone();
+			let ext = backup.extension()
+				.map(|ext| format!("{}.bak", ext.to_string_lossy()))
+				.unwrap_or_else(|| format!(".bak"));
+			backup.set_extension(&ext);
+			backup
+		};
+
+		println!("Moving \"{}\" to \"{}\"", path.display(), backup.display());
+		std::fs::rename(&path, backup)?;
+	}
 
 	println!("Creating file \"{}\"", path.display());
-	let mut file = options.open(&path)?;
+	let mut file = std::fs::OpenOptions::new()
+		.create_new(true)
+		.open(&path)?;
 
 	gl.artifact(
 		project.id,
 		&deploy.ref_name,
-        &deploy.file,
-        &deploy.job,
+		&deploy.file,
+		&deploy.job,
 		&mut file,
 	)?;
 
